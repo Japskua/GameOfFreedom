73f509f3c78be8201fc84aac11e71d6fe7cff1b6
Janne Parkkila <japskua@gmail.com>
2012-02-04 18:20:58 +0200

    Created the server class, that listens to the given port at loopback address. In addition, modified the main.py so that it reads the commandline arguments and starts the server accordingly

---
+++ main.py
@@ -0,0 +1,39 @@
+'''
+Created on Feb 4, 2012
+
+@author: Japskua
+'''
+
+import server
+import getopt
+import sys
+
+version = '1.0'
+verbose = True
+port = 100
+host = "100.0"
+
+output_filename = 'output.log'
+
+## Get the command line arguments
+options, remainder = getopt.getopt(sys.argv[1:], 'h:p:v', ['host=', 
+                                                         'verbose',
+                                                         'port=',
+                                                         ])
+
+# Check if all the values are defined
+for opt, arg in options:
+    if opt in ('-h', '--host'):
+        host = arg
+    elif opt in ('-v', '--verbose'):
+        verbose = True
+    elif opt in ("-p", '--port'):
+        port = int(arg)
+
+if verbose:
+    print "Starting the server..."
+
+# Create the server class and put it to run
+freedom_server = server.Server(port, verbose)
+freedom_server.ShowArgs()
+freedom_server.StartServer()
---
+++ server/server.py
@@ -0,0 +1,65 @@
+'''
+Created on Feb 4, 2012
+
+@author: japskua
+'''
+
+import socket
+
+ip = "127.0.0.1"
+
+# Buffer size
+buffer_size = 512
+
+class Server(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, port, verbose):
+        '''
+        Constructor
+        @param port: Port to which the server listens to
+        @type port: Integer
+        @param verbose: If this value is set to True, the server
+                        displays additional information
+        @type verbose: Boolean
+        '''
+        self.port = port
+        self.verbose = verbose
+        
+        if self.verbose:
+            print "Server Created Successfully!"
+        
+    def ShowArgs(self):
+        """
+        Displays the important values of the server
+        """
+        print "Port=", self.port
+        
+    def StartServer(self):
+        """
+        Creates all the server required stuff (e.g. creating
+        and binding the sockets, etc.)
+        
+        Starts the server main loop
+        """
+        
+        # Create the socket for the datagram
+        sock = socket.socket(socket.AF_INET,    #IPv4
+                             socket.SOCK_DGRAM) #UDP
+        
+        # Bind the socket
+        sock.bind( (ip, self.port) )
+        
+        if self.verbose:
+            print "Server ready to listen"
+        
+        # Start running
+        while True:
+            data, addr = sock.recvfrom(buffer_size)
+            print "Received message:", data
+            print "From:", addr
+        
+        
